9b2f3178929aaaf1163e7a58cae750b1d43c58cd
Maximilian Adam <maxjadam@icloud.com>
2024-03-29 19:49:28 +0000

    website implemented

--- assets/js/main.js
+++ assets/js/main.js
@@ -194,24 +194,33 @@
    * Initiate portfolio lightbox 
    */
   const portfolioLightbox = GLightbox({
-    selector: '.portfolio-lightbox'
+    selector: '.portfolio-lightbox',
+  });
+
+  const portfolioDetailsLightbox = GLightbox({
+    selector: '.portfolio-details-lightbox',
+    width: '90%',
+    height: '90vh',
   });
 
   /**
    * Portfolio details slider
    */
   new Swiper('.portfolio-details-slider', {
-    speed: 400,
+    speed: 800,
     loop: true,
     autoplay: {
-      delay: 5000,
+      delay: 4000,
       disableOnInteraction: false
     },
     pagination: {
       el: '.swiper-pagination',
       type: 'bullets',
       clickable: true
-    }
+    },
+    centeredSlides: true,
+    slidesPerView: 1,
+    spaceBetween: 30,
   });
 
   /**
